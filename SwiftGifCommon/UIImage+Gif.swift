463de76612edcb1fef25839009615f4f53f609d7
Dev Sanghani <devsanghani@gmail.com>
2018-05-29 13:11:21 +0530

    Add @objc to expose public methods in objc

--- SwiftGifCommon/UIImage+Gif.swift
+++ SwiftGifCommon/UIImage+Gif.swift
@@ -11,7 +11,7 @@ import ImageIO
 
 extension UIImageView {
 
-    public func loadGif(name: String) {
+    @objc public func loadGif(name: String) {
         DispatchQueue.global().async {
             let image = UIImage.gif(name: name)
             DispatchQueue.main.async {
@@ -21,7 +21,7 @@ extension UIImageView {
     }
 
     @available(iOS 9.0, *)
-    public func loadGif(asset: String) {
+    @objc public func loadGif(asset: String) {
         DispatchQueue.global().async {
             let image = UIImage.gif(asset: asset)
             DispatchQueue.main.async {
@@ -34,7 +34,7 @@ extension UIImageView {
 
 extension UIImage {
 
-    public class func gif(data: Data) -> UIImage? {
+    @objc public class func gif(data: Data) -> UIImage? {
         // Create source from data
         guard let source = CGImageSourceCreateWithData(data as CFData, nil) else {
             print("SwiftGif: Source for the image does not exist")
@@ -44,7 +44,7 @@ extension UIImage {
         return UIImage.animatedImageWithSource(source)
     }
 
-    public class func gif(url: String) -> UIImage? {
+    @objc public class func gif(url: String) -> UIImage? {
         // Validate URL
         guard let bundleURL = URL(string: url) else {
             print("SwiftGif: This image named \"\(url)\" does not exist")
@@ -60,7 +60,7 @@ extension UIImage {
         return gif(data: imageData)
     }
 
-    public class func gif(name: String) -> UIImage? {
+    @objc public class func gif(name: String) -> UIImage? {
         // Check for existance of gif
         guard let bundleURL = Bundle.main
           .url(forResource: name, withExtension: "gif") else {
@@ -78,7 +78,7 @@ extension UIImage {
     }
 
     @available(iOS 9.0, *)
-    public class func gif(asset: String) -> UIImage? {
+    @objc public class func gif(asset: String) -> UIImage? {
         // Create source from assets catalog
         guard let dataAsset = NSDataAsset(name: asset) else {
             print("SwiftGif: Cannot turn image named \"\(asset)\" into NSDataAsset")
